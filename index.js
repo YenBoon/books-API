69eb385f040916c2896b79918231b5485a980c20
Ben Yoon <BYoon12@gmail.com>
2023-02-08 19:13:35 -0500

    fixed index.js code

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+  res.send('Hello, world!')
 })
 
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
-app.listen(process.env.PORT)
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
+
+module.exports = app;
